97b485e64b5d977ff2a9a75b37cac1c7cfe53e59
koudaiii <cs006061@gmail.com>
2014-04-13 19:30:45 +0900

    first commit

---
+++ setup_mysql.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+#Run mysql user
+chown -R mysql:mysql /var/lib/mysql
+
+#Install
+mysql_install_db --user mysql > /dev/null
+
+sleep 10s
+
+#Start mysqld 
+/etc/init.d/mysqld start &
+MYSQL_PID=$!
+wait $MYSQL_PID
+
+sleep 10s
+
+#Setup MYSQL
+MYSQL_COMMAND="mysql -uroot mysql -e"
+
+$MYSQL_COMMAND "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%'  WITH GRANT OPTION;"
+$MYSQL_COMMAND "CREATE DATABASE your_database ;"
+$MYSQL_COMMAND "FLUSH PRIVILEGES;"
+
